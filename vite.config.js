d378b1a8c08d34b209fe031935413ebe17ba199d
Fatema-Mithaiwala <115212706+Fatema-Mithaiwala@users.noreply.github.com>
2024-05-21 07:12:36 +0530

    deplpy-push-vercel: config change for build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // The output directory for the build.
+    assetsDir: 'assets',  // Directory for static assets.
+  },
   assetsInclude: ["**/*.jpg", "**/*.png"]
 })
